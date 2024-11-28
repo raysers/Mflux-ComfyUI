64593148ec1052d9b4ea96f7e25e0e121e4a7285
raysers <46228089+raysers@users.noreply.github.com>
2024-11-28 00:21:51 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from .Mflux_Comfy.Mflux_Air import QuickMfluxNode, MfluxModelsLoader, MfluxModelsDownloader, MfluxCustomModels
-from .Mflux_Comfy.Mflux_Pro import MfluxLoadImage, MfluxLorasLoader, MfluxControlNetLoader  # 仅导入所需的非实验节点
+from .Mflux_Comfy.Mflux_Pro import MfluxImg2Img, MfluxLorasLoader, MfluxControlNetLoader  # 仅导入所需的非实验节点
 
 NODE_CLASS_MAPPINGS = {
     "QuickMfluxNode": QuickMfluxNode,
     "MfluxModelsLoader": MfluxModelsLoader,
     "MfluxModelsDownloader": MfluxModelsDownloader,
     "MfluxCustomModels": MfluxCustomModels,
-    "MfluxLoadImage": MfluxLoadImage,
+    "MfluxImg2Img": MfluxImg2Img,
     "MfluxLorasLoader": MfluxLorasLoader,
     "MfluxControlNetLoader": MfluxControlNetLoader,
 }
@@ -16,7 +16,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "MfluxModelsLoader": "MFlux Models Loader",
     "MfluxModelsDownloader": "MFlux Models Downloader",
     "MfluxCustomModels": "MFlux Custom Models",
-    "MfluxLoadImage": "MFlux Load Image",
+    "MfluxImg2Img": "Mflux Img2Img",
     "MfluxLorasLoader": "MFlux Loras Loader",
     "MfluxControlNetLoader": "MFlux ControlNet Loader",
 }
